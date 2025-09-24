105b255b3e436b2344f1a2c55d5438986a50a268
kirtan-sharma <kirtansharma2007@gmail.com>
2025-09-24 19:56:01 +0530

    Added

---
+++ Table.loop.py
@@ -0,0 +1,16 @@
+#For loop
+
+n=int(input("Enter value:"))
+for i in range(1,11):
+	print(n*i)
+print("This is by for loop")
+
+
+#While loop
+
+n=int(input("Enter value:"))
+s=1
+while(s<=10):
+	print(n*s)
+	s=s+1
+print("This is by while loop")

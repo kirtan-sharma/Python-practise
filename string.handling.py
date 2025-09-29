2ff252fea2e58eaf6da33bababd07edd997d6649
kirtan-sharma <kirtansharma2007@gmail.com>
2025-09-29 20:20:52 +0530

    Added

---
+++ string.handling.py
@@ -0,0 +1,22 @@
+a="Kirtan"
+for i in range(len(a)):
+	print(a[i])
+
+#slicing(index based process)
+#a=start
+#b=ending
+#c=increament decreament
+
+a="Kirtan's laptop"
+b=a[0:6]
+print(b)
+c=a[0:6:2]
+print(c)
+
+
+#reversed string
+
+
+n=input("Enter your name:")
+b=n[::-1]
+print(b)
